6d8faf2b2f5871cba40c64e4495dc96560febfad
Albrecht Naumann <albrecht.naumann@posteo.de>
2023-12-12 07:40:45 +0100

    test for fibonacci added

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,10 +1,20 @@
-pub fn fibonacci(to: u32) -> Vec<u32>{
+pub fn fibonacci(durations: u32) -> Vec<u32>{
     let mut vec = vec![0, 1];
     let mut i = 0;
-    while i < to {
+    while i < durations {
         let next = vec[vec.len() - 1] + vec[vec.len() - 2];
         vec.push(next);
         i += 1;
     }
     return vec;
+}
+
+#[cfg(test)] 
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_fibonacci() {
+        assert_eq!(fibonacci(10), vec![0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ mod fibonacci;
 use fibonacci::fibonacci;
 
 fn main() {
-  // TODO: add a test
   fibonacci(10).iter().for_each(|x| println!("{}", x));
 }
