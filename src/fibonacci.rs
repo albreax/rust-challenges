5872ef2af17dd53fc3d491790f305eec64693309
Albrecht Naumann <albrecht.naumann@posteo.de>
2023-12-12 21:08:10 +0100

    remove unnecessary return

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,12 +1,12 @@
 pub fn fibonacci(durations: u32) -> Vec<u32>{
-    let mut vec = vec![0, 1];
-    let mut i = 0;
-    while i < durations {
-        let next = vec[vec.len() - 1] + vec[vec.len() - 2];
-        vec.push(next);
-        i += 1;
-    }
-    return vec;
+  let mut vec = vec![0, 1];
+  let mut i = 0;
+  while i < durations {
+      let next = vec[vec.len() - 1] + vec[vec.len() - 2];
+      vec.push(next);
+      i += 1;
+  }
+  vec
 }
 
 #[cfg(test)] 
