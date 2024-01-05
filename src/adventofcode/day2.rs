32fc71db11cb5fbf4758e7ef59339cdb6e4ec81c
Albrecht Naumann <albrecht.naumann@posteo.de>
2024-01-06 00:28:24 +0100

    Day 2 Challenge Part 2

--- src/adventofcode/day2.rs
+++ src/adventofcode/day2.rs
@@ -7,42 +7,74 @@ const BLUE: u8 = 14;
 pub fn day2() {
   println!("day2");
   let lines = utils::read_file("src/adventofcode/example2.txt");
-  let result = process_line(&lines);
-  println!("{:?}", sum_up(result));
+  let result = line_number(&lines);
+  println!("{:?}", result.iter().map(|&x| x as u32).sum::<u32>());
+  println!("{:?}", line_power(&lines).iter().sum::<u32>());
 }
 
-fn sum_up(vec: Vec<u8>) -> u16 {
-  vec.iter().map(|&v| v as u16).sum()
+fn line_to_parts(line: &str) -> Vec<&str> {
+  line.split(" ").collect::<Vec<&str>>()
 }
 
-fn process_line(lines: &Vec<String>) -> Vec<u8> {
+fn clean_part(part: &str) -> String {
+  part.replace(',',"")
+    .replace(';',"")
+    .replace(':', "")
+}
+
+fn line_number(lines: &Vec<String>) -> Vec<u8> {
   lines.iter().map(|line| {
-    let parts = line.split(" ").collect::<Vec<&str>>();
+    let parts = line_to_parts(line);
     let game:u8 = parts[1].replace(':',"").parse().unwrap();
     
     let mut num:u8 = 0;
-    let mut is_valid = true;
-
-    parts.iter().skip(2).for_each(|p|{
-      let part = p.replace(',',"")
-        .replace(';',"")
-        .replace(':', "");
+    let is_valid = parts.iter().skip(2).fold(true, |acc, p|{
+      let part = clean_part(p);
 
       if let Some(n) = part.parse().ok() {
         num = n;
-      } else {
-        if 
+        return acc;
+      }
+
+      match  
           part == "red" && num > RED || 
           part == "green" && num > GREEN || 
           part == "blue" && num > BLUE 
-        { is_valid = false; }
+      {
+        true => return false,
+        false => return acc,
       }
     });
-
+    
     match is_valid {
         true => game,
         false => 0,
     }
+  }).collect()
+}
+
+fn line_power(lines: &Vec<String>) -> Vec<u32> {
+  lines.iter().map(|line| {
+    let parts = line_to_parts(line);
+    
+    let mut num:u8 = 0;
+
+    let (r, g, b) = parts.iter().skip(2).fold((0 , 0, 0), |acc, p| {
+      let part = clean_part(p);
+      
+      if let Some(n) = part.parse().ok() {
+        num = n;
+        return acc;
+      } 
+
+      match part.as_str() {
+        "red" => if acc.0 < num { (num, acc.1, acc.2) } else { acc },
+        "green" => if acc.1 < num { (acc.0, num, acc.2) } else { acc },
+        "blue" => if acc.2 < num { (acc.0, acc.1, num) } else { acc },
+        _ => panic!("Invalid color")
+      }
+    });
+    r as u32 * g as u32 * b as u32
   }).collect()
 }
 
@@ -51,7 +83,7 @@ mod tests {
   use super::*;
 
   #[test]
-  fn test_process_line() {
+  fn test_line_number() {
     let lines = vec![
       String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
       String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
@@ -59,14 +91,26 @@ mod tests {
       String::from("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
       String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
     ];
-    let result = process_line(&lines);
+    let result = line_number(&lines);
     assert_eq!(result, vec![1, 2, 0, 0, 5]);
   }
 
   #[test]
   fn test_sum_up() {
     let vec = vec![1, 2, 0, 0, 5];
-    let result = sum_up(vec);
-    assert_eq!(result, 8);
+    assert_eq!(vec.iter().map(|&x| x as u32 ).sum::<u32>(), 8);
+  }
+  
+  #[test]
+  fn test_line_power() {
+    let lines = vec![
+      String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+      String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
+      String::from("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
+      String::from("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
+      String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+    ];
+    let result = line_power(&lines);
+    assert_eq!(result, vec![48, 12, 1560, 630, 36]);
   }
 }
